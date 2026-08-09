02988d8e5b569834cf8b7c0278445fc18d857ed0
Kelvin <kelvindelima710@gmail.com>
2026-08-08 22:30:50 -0300

    docs: creating sample comments to remind me all the study topics lmaooo

--- backend/src/main/java/com/eduvault/auth/AuthController.java
+++ backend/src/main/java/com/eduvault/auth/AuthController.java
@@ -2,11 +2,14 @@ package com.eduvault.auth;
 
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController // isso vai retornar automaticamente um JSON ou XML
 @RequestMapping("/auth")
+// mapeia uma requisicao http p controller e metodos
+// ela eh a mãe e meio q define a URL base do nosso request rsrs
 public class AuthController {
-
+    // essa annotation vai enviar coisa p serv (eh so um POST)
     @PostMapping("/login")
+    // o request body vai trazer uma requisicao http p um objeto jaaj
     public LoginResponse login(@RequestBody LoginRequest loginRequest) {
         return new LoginResponse(
                 "token", "ADM-mock"
