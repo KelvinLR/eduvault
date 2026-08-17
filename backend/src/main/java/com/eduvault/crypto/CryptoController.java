e1e7fed75184522a5cda64381356f2b7fd356982
KelvinLR <kelvindelima710@gmail.com>
2026-08-17 19:36:52 -0300

    refactor: removing unneccessary imports

--- backend/src/main/java/com/eduvault/crypto/CryptoController.java
+++ backend/src/main/java/com/eduvault/crypto/CryptoController.java
@@ -3,10 +3,6 @@ package com.eduvault.crypto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import tools.jackson.core.ObjectReadContext;
-
-import javax.crypto.SecretKey;
-import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.Map;
 
@@ -15,7 +11,9 @@ import java.util.Map;
 public class CryptoController {
     private final CryptoService service;
 
-    public CryptoController(CryptoService service) { this.service = service; }
+    public CryptoController(CryptoService service) {
+        this.service = service;
+    }
 
     @GetMapping("/public-key")
     public Map<String, String> getPublicKey() {
